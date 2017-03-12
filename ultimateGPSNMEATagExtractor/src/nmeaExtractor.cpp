c49302a77c3f0207dd48a1660e062790addcc16e
darthpbal <Ronquillo.paul@gmail.com>
2017-03-12 01:44:00 -0700

    added functional code for extracting any GPS tag from the GPS text file.

--- ultimateGPSNMEATagExtractor/src/nmeaExtractor.cpp
+++ ultimateGPSNMEATagExtractor/src/nmeaExtractor.cpp
@@ -1,23 +1,33 @@
 #include <iostream>
 #include <cstdlib>
 #include <fstream>
+#include <string>
 using namespace std;
+
+string gpsTag = "$GPGGA,";
+string inputFileName = "srcFile.txt";
+string outputFileName = "processedGpsData.txt";
 
 int main(){
 
-    ifstream inputFile("srcFile");
+    ifstream inputFile(inputFileName.c_str());
     if(!inputFile){
         cout << "input file failed to open" << '\n';
+        exit(-1);
+    }
+    ofstream outputFile(outputFileName.c_str());
+    if(!outputFile){
+        cout << "output file failed to open" << '\n';
         exit(-1);
     }
 
 
     string line;
-	string processedLine;
 	while(!inputFile.eof()){
 		getline(inputFile, line);
-		processedLine = extractFromLine(line, colArrSize, colArray);
-		outputFile << processedLine; // write to output file
+        if(line.substr(0, gpsTag.size()) == gpsTag){
+            outputFile << line.substr(gpsTag.size(), line.size() - 1) << endl; // write to output file
+        }
 		line.clear();//empty line to clean up for next run
 	}
 
